7df2ff7cdc1c27ba90958b4f2e6ef733a9101698
mastershashank <beingshashank08@gmail.com>
2022-11-17 23:59:58 +0530

    Update storageVsMemory.sol

--- storageVsMemory.sol
+++ storageVsMemory.sol
@@ -3,13 +3,14 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract sample{
-    uint[5] public arr = [10,20,30,40,50];
+   /* uint[5] public arr = [10,20,30,40,50];
 
     function Sgetter() public returns(uint[5] memory){
         uint[5] storage brr = arr;
         brr[1]=100;
         return brr;
     }
+*/
     function Mgetter() public view returns(uint[5] memory){
         uint[5] memory brr = arr;
         brr[1]=200;
