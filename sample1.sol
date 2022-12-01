990eb51277c925fd06d49ecdc00bdf3a2705d975
mastershashank <beingshashank08@gmail.com>
2022-12-01 23:58:06 +0530

    Initial Commit

---
+++ sample1.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract A {
+
+
+function f1() public pure returns (string memory) {
+    string memory text ="I'm in A";
+    return text;
+
+}
+
+
+
+
+}
+
