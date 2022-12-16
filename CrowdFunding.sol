3e41d85fcecc3cdd711f68c9e5319d815ae2f09b
mastershashank <beingshashank08@gmail.com>
2022-12-16 16:02:03 +0530

    Initial Commit

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -1,117 +1,93 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
 
-pragma solidity ^0.8.7;
+contract CrowdFunding{
 
-contract CrowdFunding {
-    address public owner; // owner of the contract
-    uint256 public minimumContribution; // minimum contribution required for contributor to do crowd funding.
-    uint256 public deadline; // Final Time to do crowdfunding.
-    uint256 public target;
-    uint256 public raisedAmount; // Keep track of the total raised amount.
-    uint256 public totalContributors; // total contributors who have contributed to crowd funding.
-    uint256 public numRequests;
-
-    struct Request {
-        string description;
-        address payable recipient;
-        uint256 value;
-        bool completed;
-        uint256 noOfVoters;
-        mapping(address => bool) voters;
-    }
-
-    mapping(uint256 => Request) public allRequests;
-    mapping(address => uint256) public contributors; // key value pair of contributors address and fund they have contributed.
-
-    constructor(uint256 _target, uint256 _deadline) {
-        owner = msg.sender;
-        deadline = block.timestamp + _deadline;
-        target = _target;
-        minimumContribution = 100 wei;
-    }
-
-    // All Modifiers starts here --------------------------------------------------
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can access this function.");
-        _;
-    }
-
-    modifier isDeadlinePassed() {
-        require(
-            block.timestamp < deadline,
-            "Crowd Funding deadline has passed. Please try again later."
-        );
-        _;
-    }
-
-    modifier isContributor() {
-        require(contributors[msg.sender] > 0, "Sorry, you are not a contributor. Try to contribute to crowd funding then try again. Thanks.");
-        _;
-    }
-
-    // All Modifiers Ends here --------------------------------------------------
-
-    // get balance
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
-    }
-
-    // Contribute
-    function contribute() public payable isDeadlinePassed {
-        require(
-            msg.value >= minimumContribution,
-            "Minimum 100 wei is required to contribute."
-        );
-
-        if (contributors[msg.sender] == 0) {
-            totalContributors++;
-        }
-
-        contributors[msg.sender] += msg.value;
-        raisedAmount += msg.value;
-    }
-
-    // Refund the money if the target is not fulfilled and deadline has passed.
-    function refund() public isDeadlinePassed isContributor {
-        require(raisedAmount < target, "You are not eligible for refund");
-        require(contributors[msg.sender] > 0);
-
-        address payable user = payable(msg.sender);
-        user.transfer(contributors[msg.sender]);
-    }
-
-    // Create Request
-    function createRequest(
-        string memory _description,
-        address payable _recipient,
-        uint256 _value
-    ) public onlyOwner {
-        Request storage newRequest = allRequests[numRequests];
-        numRequests++;
-
-        newRequest.description = _description;
-        newRequest.recipient = _recipient;
-        newRequest.value = _value;
-        newRequest.completed = false;
-        newRequest.noOfVoters = 0;
-    }
-
-    // vote request.
-    function voteRequest(uint256 _requestNo) public isContributor {
-        Request storage currentRequest = allRequests[_requestNo];
-        require(currentRequest.voters[msg.sender] == false, "you have already voted");
-        currentRequest.voters[msg.sender] = true;
-        currentRequest.noOfVoters++;
-    }
-
-    // make payemnt only if the voters are greater than half of the contributors.
-    function makePayment(uint _requestNo) public onlyOwner{
-        require(raisedAmount >= target);
-        Request storage currentRequest = allRequests[_requestNo];
-        require(currentRequest.completed == false, "This request has already completed");
-        require(currentRequest.noOfVoters > totalContributors/2, "Contributor does not support. Votes are low.");
-        currentRequest.recipient.transfer(currentRequest.value);
-
-
-    }
+	mapping(address=>uint) public contributors;
+	address public manager;
+	uint public minimumContribution;
+	uint public deadline;
+	uint public target;
+	uint public raisedAmount;
+	uint public noOfContributors;
+	
+	struct Request{
+	string description;
+	address payable recipient;
+	uint value;
+	bool completed;
+	uint noOfVoters;
+	mapping(address=>bool) voters;
+	
+	}
+	mapping(uint=>Request) public requests;
+	uint public numRequests;
+	
+    constructor(uint _target, uint _deadline) public
+    {
+	target = _target;
+	deadline = block.timestamp+_deadline;
+	minimumContribution = 100 wei;
+	manager=msg.sender;
+	
 }
+
+function sendEth() public payable{
+	require(block.timestamp < deadline, "Deadline has passed");
+	require(msg.value >=minimumContribution,"Minimum Contribution is not met");
+	
+	if(contributors[msg.sender]==0){
+	noOfContributors++;
+	}
+	contributors[msg.sender]+=msg.value;
+	raisedAmount+=msg.value;
+}
+	function getContractBalance() public view returns(uint) {
+	return address(this).balance;
+	}
+	
+	function refund() public{
+	require(block.timestamp>deadline && raisedAmount<target,"You not eligible for refund");
+	require(contributors[msg.sender]>0);
+	address payable user = payable(msg.sender);
+	user.transfer(contributors[msg.sender]);
+	user.transfer(contributors[msg.sender]);
+	contributors[msg.sender]=0;
+	
+}
+	modifier onlyManager(){
+		require(msg.sender==manager,"Only manager can call this function");
+		_;
+	}
+	
+	function createRequests(string memory _description,address payable _recipient, uint _value) public onlyManager{
+		Request storage newRequest = requests[numRequests];
+		numRequests++;
+		newRequest.description=_description;
+		newRequest.recipient=_recipient;
+		newRequest.value=_value;
+		newRequest.completed=false;
+		newRequest.noOfVoters=0;
+	}
+	
+	function voteRequest(uint _requestNo) public{
+		require(contributors[msg.sender]>0,"You Must be the contributor");
+		Request storage thisRequest=requests[_requestNo];
+		require(thisRequest.voters[msg.sender]==false,"You have already voted");
+		thisRequest.voters[msg.sender]=true;
+		thisRequest.noOfVoters++;
+		
+	}
+	
+	function makePayment(uint _requestNo) public onlyManager{
+		require(raisedAmount>=target);
+		Request storage thisRequest=requests[_requestNo];
+		require(thisRequest.completed==false,"The request has been completed");
+		require(thisRequest.noOfVoters > noOfContributors/2,"The Majority does not support");
+		thisRequest.recipient.transfer(thisRequest.value);
+		thisRequest.completed=true;
+	}
+	
+}
+
+	
