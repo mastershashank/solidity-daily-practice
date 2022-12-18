29a034995d30b984d89585642a4ccef75d9ae620
mastershashank <beingshashank08@gmail.com>
2022-12-18 23:44:52 +0530

    Initial Commit

---
+++ basics1.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract demo {
+    int number;
+
+    constructor(){
+        number = 5;
+    }
+
+    function getter() public view returns(int)
+    {
+        return number;
+    }
+
+    function increment() public 
+    {
+        number = number+1;
+    }
+}
