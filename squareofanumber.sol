36eb9a72ff3f93c29ed3e59abe1085acf937202c
mastershashank <beingshashank08@gmail.com>
2023-01-08 00:13:21 +0530

    Initial Commit

---
+++ squareofanumber.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract First{
+    uint public number=5000; 
+function setter( uint _a) public{
+number=_a*_a;
+
+    
+}
