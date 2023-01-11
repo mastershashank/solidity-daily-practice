819bf98c51e1982f514971851f0492c9f6e5c1d2
mastershashank <beingshashank08@gmail.com>
2023-01-11 21:29:53 +0530

    Initial Commit

---
+++ payableImpldemo.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract demo{
+    uint public number=1000; 
+function setter( uint _a) public payable{
+number=_a;
+
+    
+}
